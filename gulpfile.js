2d1e16f3f8135f0091ce7e81be76ff2da62fcd02
Braulio <braulio.diez@gmail.com>
2016-02-02 13:03:35 +0100

    duplicated required

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,6 @@ var gulp = require('gulp');
 
 var config = require('./gulp.config');
 var browser = require('./gulp.browser');
-var browser = require('./gulp.build');
+var build = require('./gulp.build');
 
 gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
