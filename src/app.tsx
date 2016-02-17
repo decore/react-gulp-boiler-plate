ceab52b7bab917e95bdbb93353473d7b7d387289
Braulio Diez <braulio.diez@gmail.com>
2016-02-17 21:59:18 +0100

    Migrated to webpack, npm start to run, pending to copy index.html to dist

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,13 @@
 //require("./usersPage.tsx")
 //import { ControllerBase } from '../../../core/controllerBase';
 import * as React from 'react';
-import ReactDOM = require('react-dom')
+//import ReactDOM = require('react-dom')
 
 // Render a simple React h1 component into the body.
 // http://stackoverflow.com/questions/33980336/property-render-does-not-exist-on-type-typeof-react
 // I have to update react version, this is not ok omg !
 // check from the other seed the versions
-ReactDOM.render(<h1>Hello, TypeScript!</h1>, document.getElementsByTagName('body')[0]);
+React.render(<h1>Hello, TypeScript!</h1>, document.getElementsByTagName('body')[0]);
 
 /*
 // <usersPage/>
