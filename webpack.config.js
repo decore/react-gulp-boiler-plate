0ebda8f17107efe1f705d375eadf3480b4506e0a
nasdan <daniel.sanchez@lemoncode.net>
2016-02-19 08:28:12 +0100

    Remove babel-loader from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -21,11 +21,6 @@ module.exports = {
   module: {
 		loaders: [
 			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: "babel-loader"
-			},
-      {
         test: /\.ts$/,
         exclude: /node_modules/,
         loader: 'ts-loader'
