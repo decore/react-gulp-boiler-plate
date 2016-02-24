19106f65ce1b8b665600d76e1392af2650586e9d
Braulio <braulio.diez@gmail.com>
2016-02-24 13:44:45 +0100

    using index.ts (common entry point reducers)

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import myApp  from '../reducers/main';
+import myApp  from '../reducers';
 import Header from './common/header';
 import ContainerContributorsPage from './contributors/contributorsPage';
 
